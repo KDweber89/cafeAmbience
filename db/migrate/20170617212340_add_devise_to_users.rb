dbff4378de36587ddbce1f7f70ca061357317c29
kurt weber <kurt@zipnosis.com>
2017-06-18 15:27:24 -0500

    minor bug with getting devise all set up right

--- db/migrate/20170617212340_add_devise_to_users.rb
+++ db/migrate/20170617212340_add_devise_to_users.rb
@@ -1,8 +1,4 @@
 class AddDeviseToUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :email, :string
-  end
-
   def self.up
     change_table :users do |t|
       ## Database authenticatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617212340) do
+ActiveRecord::Schema.define(version: 20170618202527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170617212340) do
     t.string "first_name"
     t.string "last_name"
     t.integer "age"
+    t.string "email"
   end
 
 end
