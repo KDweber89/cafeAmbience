a19fd74598d1f684f7da42ac75e351ef33193850
kurt weber <kurt@zipnosis.com>
2017-06-26 20:40:31 -0500

    lots of styling, minor additions to the DB

---
+++ db/migrate/20170627011217_adding_more_fields_to_cafes.rb
@@ -0,0 +1,9 @@
+class AddingMoreFieldsToCafes < ActiveRecord::Migration[5.1]
+  def change
+    add_column :cafes, :street_address, :string
+    add_column :cafes, :state, :string
+    add_column :cafes, :editors_note, :string, limit: 300
+    add_column :cafes, :website, :string
+    add_column :cafes, :type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618210434) do
+ActiveRecord::Schema.define(version: 20170627011217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20170618210434) do
     t.string "name"
     t.text "description"
     t.integer "user_id"
+    t.string "street_address"
+    t.string "state"
+    t.string "editors_note", limit: 300
+    t.string "website"
+    t.string "type"
   end
 
   create_table "users", force: :cascade do |t|
