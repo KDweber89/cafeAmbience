f3cfd4a7577448038d7d6c54ef308ad7b14ba3df
kurt weber <kurt@zipnosis.com>
2017-07-01 23:57:22 -0400

    more css work, got images almost finished

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -3,6 +3,7 @@ class Cafe < ApplicationRecord
 
   # validates_presence_of :name, :street_address, :state, :editors_note, :website
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "1000x1000>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
+
