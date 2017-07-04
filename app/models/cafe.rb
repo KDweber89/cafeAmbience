bab9687cbe4ac5958cac874a5b3c34bb3c380c30
kurt weber <kurt@zipnosis.com>
2017-07-04 09:30:34 -0400

    fixed the login logout problem

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -3,7 +3,6 @@ class Cafe < ApplicationRecord
 
   # validates_presence_of :name, :street_address, :state, :editors_note, :website
 
-  has_attached_file :image, styles: { medium: "1000x1000>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "1000x1000#", medium:"200x100#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
-
