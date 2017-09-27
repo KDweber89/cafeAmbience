1f26fefe68bb1b2966da2ad8b95999c1751909d0
kurt weber <kurt@zipnosis.com>
2017-09-27 18:38:23 -0500

    trying to figure out the age validator situation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
-class User < ApplicationRecord
-  # attr_accessor :encrypted_password
+class User < ActiveRecord::Base
+  validate :age_restriction
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -11,23 +9,24 @@ class User < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  # before_action :old_enough, only: [:create]
-
-  # validates :old_enough_check, date: true
-
-  # binding.pry
-
-  def age
-    age = User.birth_date
-  end
-
-  def old_enough
-    (Date.strptime(age, '%Y/%m/%d') + 18.years) < Date.today
-  end
-
-  def old_enough_check
-    if old_enough
+  def age_restriction
+    if (birth_date.to_date + 18.years) < Date.today # assuming dob format is mm/dd/yy
       errors.add :birth_date, 'must be older than 18'
     end
   end
 end
+
+
+  # def age
+  #   age = User.birth_date
+  # end
+
+  # def old_enough
+  #   (Date.strptime(age, '%Y/%m/%d') + 18.years) < Date.today
+  # end
+
+  # def old_enough_check
+  #   if old_enough
+  #     errors.add :birth_date, 'must be older than 18'
+  #   end
+  # end
