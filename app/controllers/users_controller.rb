e2d800620783678b5cf15f9e366a530f4745f931
kurt weber <kurt@zipnosis.com>
2017-11-24 13:46:22 -0600

    altered the devise edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:birth_date)
+    params.require(:user).permit(:birth_date, :image)
   end
 end
